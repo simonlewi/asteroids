3befb0366c66417015689b6136f393a775b1f326
simonlewi <simon.wilen@gmail.com>
2024-12-06 22:17:06 +0100

    Added delta_time for resource efficiency

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     print("Starting asteroids!")
     print_screen_width_height()
 
+    clock = pygame.time.Clock()
+    delta_time = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -22,6 +25,7 @@ def main():
         
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        delta_time = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
