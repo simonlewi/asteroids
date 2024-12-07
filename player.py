0ed8024b4e3c3ad6ab896ccbddd79f03587dea8b
simonlewi <simon.wilen@gmail.com>
2024-12-07 18:16:34 +0100

    Added feature of player not going out of bounds

--- player.py
+++ player.py
@@ -2,7 +2,14 @@
 
 from circleshape import *
 
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import (
+    PLAYER_RADIUS, 
+    PLAYER_TURN_SPEED, 
+    PLAYER_SPEED,
+    PLAYER_BOUNDARY_OFFSET, 
+    SCREEN_WIDTH, 
+    SCREEN_HEIGHT,
+    )
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -20,8 +27,21 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+    # Player rotation with A, D
     def rotate(self, dt):
         self.rotation = self.rotation + (PLAYER_TURN_SPEED * dt)
+    
+    # Player movement back and forth
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        new_position = self.position + (forward * PLAYER_SPEED * dt)
+
+    # Move only if within bounds (extra feature)
+        if (0 <= new_position.x <= SCREEN_WIDTH - PLAYER_BOUNDARY_OFFSET and 0 <= new_position.y <= SCREEN_HEIGHT - PLAYER_BOUNDARY_OFFSET):
+            self.position = new_position
+            
+        
+
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -30,5 +50,10 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
 
         
